e431de83e6b166422d1f1ed514576fb6ff003862
Krishna20101 <jaishreekrishna011985@gmail.com>
2026-05-05 16:59:49 +0530

    Modify API credentials in vars.py
    Updated API_ID, API_HASH, and BOT_TOKEN values.

--- vars.py
+++ vars.py
@@ -2,9 +2,9 @@
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
-API_ID    = os.environ.get("API_ID", "28578880")
-API_HASH  = os.environ.get("API_HASH", "5f8c87efde57e01d12c0ce98ffdf5928")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID    = os.environ.get("API_ID", "22173951")
+API_HASH  = os.environ.get("API_HASH", "617f91fd4b920559eb67eb956afc20c3)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8605008899:AAEvqMoxglCax2NaO_5brxuqknJFdpAnU9M")
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
 
